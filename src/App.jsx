7aac0a996d2124fbb32ab43317ec0d4899bf88a2
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-04-23 14:16:05 -0300

    Add 3 array's of objects for the form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import CustomInput from "./components/customInput";
+import CustomForm from "./components/CustomForm";
 
-function App() {
-  const [count, setCount] = useState(0)
+const personalInputs = [
+  {
+    label: "Full name",
+    placeHolder: "First and last name",
+    id: "full-name",
+    type: "text",
+  },
+  {
+    label: "Email",
+    placeHolder: "Enter email",
+    id: "email",
+    type: "email",
+  },
+  {
+    label: "Phone number",
+    placeHolder: "Enter phone number",
+    id: "phone",
+    type: "tel",
+  },
+  {
+    label: "Address",
+    placeHolder: "City or Country",
+    id: "Address",
+    type: "text",
+  },
+];
+const educationInputs = [
+  {
+    label: "School",
+    placeHolder: "Enter school or university",
+    id: "school",
+    type: "text",
+  },
+  {
+    label: "Degree",
+    placeHolder: "Enter degree or Field of study",
+    id: "degree",
+    type: "text",
+  },
+  {
+    label: "Start date",
+    placeHolder: "Enter start date",
+    id: "education-start-date",
+    type: "date",
+  },
+  {
+    label: "End date",
+    placeHolder: "Enter end date",
+    id: "education-end-date",
+    type: "date",
+  },
+];
+const experienceInputs = [
+  {
+    label: "Company name",
+    placeHolder: "Enter company name",
+    id: "company",
+    type: "text",
+  },
+  {
+    label: "Position title",
+    placeHolder: "Enter position title",
+    id: "position",
+    type: "text",
+  },
+  {
+    label: "Start date",
+    placeHolder: "Enter start date",
+    id: "experience-start-date",
+    type: "date",
+  },
+  {
+    label: "End date",
+    placeHolder: "Enter end date",
+    id: "experience-end-date",
+    type: "date",
+  },
+  {
+    label: "Description",
+    placeHolder: "Enter description",
+    id: "description",
+    type: "text",
+  },
+];
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h2>Personal details</h2>
+      <CustomForm inputs={personalInputs}></CustomForm>
 
-export default App
+      <h2>Education</h2>
+      <CustomForm inputs={educationInputs}></CustomForm>
+
+      <h2>Experience</h2>
+      <CustomForm inputs={experienceInputs}></CustomForm>
+    </>
+  );
+}
