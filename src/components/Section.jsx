1b91eaf0ca1f9fa6cb97cb0cd5417f7009646748
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-02 18:48:37 -0300

    update component to render various sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,24 +1,30 @@
 export default function Section({ data, title }) {
   return (
-    <section className="resume-section">
-      <h3 className="section-title">{title}</h3>
-      <div className="section-info-container">
-        <div className="section-info-group">
-          <p>
-            {data["startDateLabel"]}
-            {(data["startDateLabel"] || data["endDateLabel"]) && " - "}
-            {data["endDateLabel"]}
-          </p>
-          <p>{data["locationLabel"]}</p>
-        </div>
-        <div className="section-info-group">
-          <p>
-            <strong>{data["institutionLabel"]}</strong>
-          </p>
-          <p>{data["positionLabel"]} </p>
-          {data["descriptionLabel"] && <p>{data["descriptionLabel"]}</p>}
-        </div>
-      </div>
-    </section>
+    <>
+      {data.length > 0 && (
+        <section className="resume-section">
+          <h3 className="section-title">{title}</h3>
+          {data.map((section, index) => (
+            <div key={index} className="section-info-container">
+              <div className="section-info-group">
+                <p>
+                  {section.startDateLabel}
+                  {(section.startDateLabel || section.endDateLabel) && " - "}
+                  {section.endDateLabel}
+                </p>
+                <p>{section.locationLabel}</p>
+              </div>
+              <div className="section-info-group">
+                <p>
+                  <strong>{section.institutionLabel}</strong>
+                </p>
+                <p>{section.positionLabel} </p>
+                {section.descriptionLabel && <p>{section.descriptionLabel}</p>}
+              </div>
+            </div>
+          ))}
+        </section>
+      )}
+    </>
   );
 }
