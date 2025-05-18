272f744ccba0e27dd4c73ee235ae93343ff4aef2
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-18 17:07:52 -0300

    add icons that only shows when there is data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,39 @@
+import { Mail, Phone, MapPin } from "lucide-react";
+
 export default function Header({ data }) {
   return (
     <header className="resume-header">
-      <h1>{data["full-name"]}</h1>
-      <div className="contact-info-container">
-        <div className="email">
-          {/* icono */}
-          <p>{data["email"]} </p>
-        </div>
-        <div className="phone">
-          {/* icono */}
-          <p>{data["phone"]} </p>
-        </div>
-        <div className="address">
-          {/* icono */}
-          <p>{data["address"]} </p>
-        </div>
-      </div>
+      {Object.keys(data).length > 0 && (
+        <>
+          <h1>{data["full-name"]}</h1>
+          <div className="contact-info-container">
+            <div className="email">
+              {data["email"] && (
+                <>
+                  <Mail />
+                  <p>{data["email"]} </p>
+                </>
+              )}
+            </div>
+            <div className="phone">
+              {data["phone"] && (
+                <>
+                  <Phone />
+                  <p>{data["phone"]} </p>
+                </>
+              )}
+            </div>
+            <div className="address">
+              {data["address"] && (
+                <>
+                  <MapPin />
+                  <p>{data["address"]} </p>
+                </>
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </header>
   );
 }
